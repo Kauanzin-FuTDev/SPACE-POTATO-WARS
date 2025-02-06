33602cbdbcb1b8b26b047339596221ca254a1ec5
kauan rocha <kauan.kr1001@gmail.com>
2025-02-06 14:16:33 -0300

    OS Tiro está saindo, musica de fundo, inimigo surgindo aleatóriamente

--- projetinho pae.py
+++ projetinho pae.py
@@ -1,143 +1,95 @@
 import pygame
 from random import randint
-from random import Random
-from time import sleep
 
+# Inicialização do pygame
 pygame.init()
 pygame.mixer.init()
 
-screen = pygame.display.set_mode((600 , 900 ))
-
+# Configurações da tela
+WIDTH, HEIGHT = 600, 900
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SPACE POTATO WARS")
 
-
+# Variáveis de controle
 shot_Scoop = False
-
-enimy_x = 230
+enimy_x = randint(50, 500)
 enimy_y = 100
-speed_enimy = 8
-
-#shoots
-x_shoot = 30
-y_shoot = 30
-speed_y_shoot = 30
-
+speed_enimy = 2
+speed_y_shoot = 20
 shoot = []
+score = 0
 
-Score = 0
-
-
-
-#Imagens e sonds
-potatoImg_Player = pygame.image.load("D:\progaming\Pygame\Baked_Potato_JE4_BE2.webp")
-BackImg = pygame.image.load("D:\progaming\Pygame\quadrodecorativoretangularplanetaterraspaceazul.webp")
-enimyPotatoImg = pygame.image.load("D:\progaming\Pygame\Poisonous_Potato_JE3_BE2.webp")
-shootImg = pygame.image.load("D:\progaming\Pygame\pngtree-golden-yellow-fast-food-potato-fries-png-image_2707911.jpg")
-shootImg = pygame.transform.scale( shootImg, (30,30)) #Mudando a escala 
-sondOfShoot = pygame.mixer.Sound("D:/progaming/Pygame/sound-effects-single-gun-shot-247124.mp3")
-
-
-
-
-
-
-
-
-
-#POSITION 
+# Posições iniciais do jogador
 player_x = 230
 player_y = 700
-
-#Movimatation
 speed_player = 10
+
+# Carregamento de imagens e sons
+potatoImg_Player = pygame.image.load("D:/progaming/Pygame/Baked_Potato_JE4_BE2.webp")
+BackImg = pygame.image.load("D:/progaming/Pygame/quadrodecorativoretangularplanetaterraspaceazul.webp")
+enimyPotatoImg = pygame.image.load("D:/progaming/Pygame/Poisonous_Potato_JE3_BE2.webp")
+shootImg = pygame.image.load("D:/progaming/Pygame/pngtree-golden-yellow-fast-food-potato-fries-png-image_2707911.jpg")
+shootImg = pygame.transform.scale(shootImg, (30, 30))
+
+sondOfShoot = pygame.mixer.Sound("D:/progaming/Pygame/sound-effects-single-gun-shot-247124.mp3")
+background_music = pygame.mixer.Sound("D:\progaming\Pygame\pad-space-travel-hyperdrive-engine-humming-235901.mp3")
+
+# Iniciar música de fundo (repetindo infinitamente)
+background_music.play(-1)
+
+# Relógio do jogo
 clock = pygame.time.Clock()
 
-
-#Enimy movimatation
-enimy_x += 10
-
-
-
-
-
-
-
+# Loop principal
 running = True
 while running:
+    screen.blit(BackImg, (0, 0))  # Atualiza o fundo
 
-
-    for event in pygame.event.get():       #fechar o pygame
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # Movimentação do jogador
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]: player_y -= speed_player
+    if keys[pygame.K_DOWN]: player_y += speed_player
+    if keys[pygame.K_LEFT]: player_x -= speed_player
+    if keys[pygame.K_RIGHT]: player_x += speed_player
 
-    #key movimatation
-    if keys[pygame.K_UP]:
-     player_y -= speed_player
-    if keys[pygame.K_DOWN]:
-        player_y += speed_player
-    if keys[pygame.K_LEFT]:
-        player_x -= speed_player
-    if keys[pygame.K_RIGHT]:
-        player_x += speed_player
+    # Limitar o jogador à tela
+    player_x = max(-22, min(player_x, 450))
+    player_y = max(-30, min(player_y, 751))
 
-    #Enimy movimatation
-    enimy_y += 2
+    # Movimento do inimigo
+    enimy_y += speed_enimy
+    if enimy_y > HEIGHT:
+        enimy_x = randint(50, 500)
+        enimy_y = randint(-100, -30)
 
-    if enimy_y > 800:
-        random_x = randint(-22,450)
-        random_y = randint(-50,-30)
-        
-        enimy_y = random_y
-        enimy_x = random_x
-    
-    
-    if keys[pygame.K_SPACE] and not shot_Scoop:  
-     shot_Scoop = True
-     sondOfShoot.play()  
-     y_shoot = player_y
-     x_shoot = player_x
-     shoot.append([player_x + 20, player_y])
+    # Disparo do jogador
+    if keys[pygame.K_SPACE] and not shot_Scoop:
+        shot_Scoop = True
+        sondOfShoot.play()
+        shoot.append([player_x + 20, player_y])
 
+    # Atualizar posição dos tiros
+    for missile in shoot[:]:  # Criar uma cópia para evitar erros de remoção
+        missile[1] -= speed_y_shoot
+        if missile[1] < -30:
+            shoot.remove(missile)  # Remove tiros que saíram da tela
+
+    # Redefinir a possibilidade de atirar após um intervalo
+    if not keys[pygame.K_SPACE]:
+        shot_Scoop = False
+
+    # Desenhar jogador, inimigo e tiros
+    screen.blit(potatoImg_Player, (player_x, player_y))
+    screen.blit(enimyPotatoImg, (enimy_x, enimy_y))
     for missile in shoot:
-        missile[1] -= speed_y_shoot  # Move o tiro para cima
-        screen.blit(shootImg, (missile[0], missile[1]))  # Desenha o tiro
+        screen.blit(shootImg, (missile[0], missile[1]))
 
-
-
-
-
-
-
- # Restringir jogador aos limites da tela
-    if player_y <= -30:
-        player_y = -30
-
-    if player_y >= 751:
-        player_y = 751
-
-    if player_x <= -22:
-        player_x = -22
-
-    if player_x >= 450:
-        player_x = 450
-
-
-
-
-    screen.blit(BackImg , (0 , 0))
-    screen.blit(potatoImg_Player , (player_x , player_y))
-    screen.blit(enimyPotatoImg , (enimy_x,enimy_y))
-    
     pygame.display.flip()
-
-
-
-    print(player_y)
-    clock.tick(60) #FPS
-
-
+    clock.tick(60)
 
 pygame.quit()
 
